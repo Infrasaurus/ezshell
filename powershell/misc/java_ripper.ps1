2d67294b9c42553ca53474d6b97817050b4ee07a
justin-sysadmin <justingn@gmail.com>
2023-12-14 22:20:51 -0500

    Updated java_ripper w/ QoL changes

--- powershell/misc/java_ripper.ps1
+++ powershell/misc/java_ripper.ps1
@@ -1,6 +1,6 @@
 ###############################################################################
 # SCRIPT NAME: Oracle JRE Ripper
-# VERSION: v0.1
+# VERSION: v0.2
 # LAST MODIFIED: 14-DEC-2023
 # AUTHOR: Infrasaurus
 ###############################################################################
@@ -18,15 +18,27 @@
 # - If Oracle JRE isn't installed properly (i.e., no registry entries), script
 # fails
 ###############################################################################
+# Creates -network switch parameter for script and assumes default is false
 param (
     [switch]$network = $false
 )
+# If network mode enabled, checks for machines.txt in present working directory
 if ($network -like "True") {
+    $machines = Test-Path -Path ".\machines.txt"
+    # If fails, prints error messages to user and ends script.
+    if ($machines -like "False") {
+        Write-Host "The required machines.txt file does not exist!"
+        Write-Host "Please make sure it's created in your current working directory with one IP or FQDN per line, then try again."
+        Write-Host "To run in local mode, please drop the -network flag."
+        Break
+    }
     $machines = Get-Content ".\machines.txt"
     ForEach ($machine in $machines) {
+        # Get-WMIObject is bad. Queries registry instead for Uninstall entries
         $AllApplications = @()
         $AllApplications += Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
         $AllApplications += Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        # Attempts to identify Orale JRE and EXCLUDE Oracle JDK
         $OracleJRE = $AllApplications | Where-Object {(($_.DisplayName) -like "Java") -and (($_.Publisher) -like "Oracle Corporation") -and (($_.DisplayName) -notlike "JDK")}
         if ($OracleJRE -ne $null) {
             Write-Host "Removing Oracle JRE from $machine..."
